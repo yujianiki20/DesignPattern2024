81770bd342b53a406cb3cb9a2339885bc7b89f09
mori yuji <y.mori@neogenia.co.jp>
2025-01-08 09:27:36 +0900

    Adapterパターン

---
+++ Adapter/Main.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Threading;
+
+namespace AdapterPatternExample
+{
+    // target
+    public interface IDisplayTimer
+    {
+        string GetFormattedTime();
+        int Countdown();
+    }
+
+    // adaptee: 
+    public class Timer
+    {
+        private int _remainingSeconds;
+        public Timer(int seconds)
+        {
+            _remainingSeconds = seconds;
+        }
+
+        public int Reduce()
+        {
+            if (_remainingSeconds > 0)
+            {
+                _remainingSeconds--;
+            }
+            return _remainingSeconds;
+        }
+
+        public int GetRemainingSeconds()
+        {
+            return _remainingSeconds;
+        }
+    }
+
+    // adapter
+    public class FormatTimer : Timer, IDisplayTimer
+    {
+        public FormatTimer(int seconds) : base(seconds)
+        {
+        }
+
+        public int Countdown()
+        {
+            return base.Reduce();
+        }
+        public string GetFormattedTime()
+        {
+            int totalSeconds = GetRemainingSeconds();
+            int hours = totalSeconds / 3600;
+            int minutes = (totalSeconds % 3600) / 60;
+            int seconds = totalSeconds % 60;
+
+            return $"{hours:D2}:{minutes:D2}:{seconds:D2}";
+        }
+    }
+
+    // main
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("秒数を入力してください");
+            if (int.TryParse(Console.ReadLine(), out int totalSeconds))
+            {
+                IDisplayTimer timer = new FormatTimer(totalSeconds);
+
+                while (timer.Countdown() > 0)
+                {
+                    Console.WriteLine(timer.GetFormattedTime());
+                    Thread.Sleep(1000); // 1秒待つ
+                }
+                Console.WriteLine("時間になりました");
+            }
+        }
+    }
+}
